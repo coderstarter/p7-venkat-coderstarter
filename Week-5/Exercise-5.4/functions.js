cdd024073ee2b2576460588d2b7e5e549478f8f8
coderstarter <102434201+coderstarter@users.noreply.github.com>
2022-12-18 20:20:49 +0530

    Create functions.js

---
+++ Week-5/Exercise-5.4/functions.js
@@ -0,0 +1,11 @@
+const functions = {
+    add: (num1, num2) => num1 + num2,
+
+    subtract: (nums1, num2) => nums1 - num2,
+
+    multiply: (num1, num2) => num1 * num2,
+
+    divide: (num1, num2) => num1 / num2
+}
+
+module.exports = functions;
