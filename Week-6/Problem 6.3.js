201454c4e71c2e438f020d9323cf69d1fb9aa559
coderstarter <102434201+coderstarter@users.noreply.github.com>
2022-12-25 22:20:31 +0530

    Create Problem 6.3.js

---
+++ Week-6/Problem 6.3.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let lo = 0,hi=nums.length-1,mid=0;
+    while(mid<=hi)
+    {
+        switch(nums[mid])
+        {
+            case 0:
+                let tmp = nums[lo];
+                nums[lo] = nums[mid];
+                nums[mid] = tmp;
+                lo++;
+                mid++;
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                let tmmp = nums[mid];
+                nums[mid] = nums[hi];
+                nums[hi] = tmmp;
+                hi--;
+                break;
+        }
+    }
+};
+
+https://leetcode.com/problems/sort-colors/submissions/865342293/
