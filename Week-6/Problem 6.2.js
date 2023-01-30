f7bcbd0b2c3a35130d367bbaa0f005cc69eec805
coderstarter <102434201+coderstarter@users.noreply.github.com>
2023-01-31 02:01:07 +0530

    Update Problem 6.2.js

--- Week-6/Problem 6.2.js
+++ Week-6/Problem 6.2.js
@@ -1,51 +1,37 @@
 /**
- * @param {number} n
- * @return {number[][]}
+ * @param {number[][]} matrix
+ * @return {number[]}
  */
-var generateMatrix = function(n) {
-    let arr = [];
-    let r1=0,r2=n-1;
-    let c1=0,c2=n-1;
-    let fill = 0;
-    for(let i=0;i<n;i++)
-    {
-        arr[i] = []
-        for(let j=0;j<n;j++){
-            arr[i][j] = 0;
+var spiralOrder = function(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let ans = [];
+    let i,k=0,l=0;
+    while(k<m && l<n){
+        for(i=l;i<n;i++){
+            ans.push(matrix[k][i]);
+        }
+        k++;
+        for(i=k;i<m;i++)
+        {
+            ans.push(matrix[i][n-1]);
+        }
+        n--;
+        if(k<m){
+            for(i=n-1;i>=0;i--){
+                ans.push(matrix[m-1][i]);
+            }
+            m--;
+        }
+        if(l<n){
+            for(i=m-1;i>=k;i--){
+                ans.push(matrix[i][l]);
+            }
+            l++;
         }
     }
-
-    let val=1;
-
-    while(r1<=r2 && c1<=c2){
-        for(let c=c1;c<=c2;c++)
-        {
-            arr[r1][c] = val;
-            val++;
-        }
-        for(let r=r1+1;r<=r2;r++)
-        {
-            arr[r][c2] = val;
-            val++;
-        }
-        if(r1<r2 && c1<c2){
-            for(let c = c2-1;c>c1;c--){
-                arr[r2][c] = val;
-                val++;
-            }
-            for(let r=r2;r>r1;r--){
-                arr[r][c1] = val;
-                val++;
-            }
-        }
-        r1++;
-        r2--;
-        c1++;
-        c2--;
-    }
-
-    return arr;
+    return ans;
 };
 
-
-https://leetcode.com/problems/spiral-matrix-ii/submissions/865231875/
+// time complexity: o(n^2) as we are iterating the matrix
+// space complexity: o(n) for storing the ans
