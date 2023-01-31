ae7b039307973243410dc51d38d402fea11a06c5
coderstarter <102434201+coderstarter@users.noreply.github.com>
2023-02-01 00:03:22 +0530

    Update solution_7.3.js

--- Week-7/solution_7.3.js
+++ Week-7/solution_7.3.js
@@ -22,3 +22,94 @@ var hasCycle = function(head) {
     }
     return false;
 };
+
+// full implementation of detecting loop.
+
+class Node {
+    constructor(ele, next) {
+        this.ele = ele;
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+class SingleLinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.size = 1;
+    }
+    append(ele) {
+        const newNode = new Node(ele);
+        var current;
+        if (this.head === null) {
+            this.head = newNode;
+        }
+        else {
+            var cur = this.head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        this.size++;
+    }
+    print() {
+        let cur = this.head;
+        while (cur != null) {
+            console.log(cur.ele + " ");
+            cur = cur.next;
+        }
+    }
+    printList() {
+        let array = ['head'];
+        let cur = this.head;
+        while (cur != null) {
+            array.push(cur.ele);
+            cur = cur.next;
+        }
+        array.push('null');
+        console.log(array.join("->"))
+    }
+    reverseLinkedList() {
+        var cur = this.head;
+        var prev = null;
+        var next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        this.head = prev;
+    }
+    creatLoop() {
+        this.head.next.next.next.next = this.head;
+    }
+    detectLoop() {
+        var slow = this.head;
+        var fast = this.head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast === slow) {
+                return "Loop is detected"
+            }
+        }
+        console.log('No loop is detected');
+    }
+}
+
+const list = new SingleLinkedList(5);
+
+list.append(10);
+list.append(40);
+list.append(20);
+list.append(12);
+// list.print();
+list.printList()
+list.creatLoop()
+console.log(list.detectLoop())
+list.printList()
+
+// time complexity : o(n) as we iterating the linkedlist
+// space complexity : o(1)
