e03dc5c444182ce1692e8779197c0fc916ffc132
coderstarter <102434201+coderstarter@users.noreply.github.com>
2023-01-31 23:52:15 +0530

    Update solution7.2.js

--- Week-7/solution7.2.js
+++ Week-7/solution7.2.js
@@ -30,4 +30,96 @@ var rotateRight = function(head, k) {
     return head;
 };
 
-submission link : https://leetcode.com/problems/rotate-list/submissions/880141085/
+// submission link : https://leetcode.com/problems/rotate-list/submissions/880141085/
+
+// full implementation 
+class Node {
+    constructor(ele, next) {
+        this.ele = ele;
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+class SingleLinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.size = 1;
+    }
+    append(ele) {
+        const newNode = new Node(ele);
+        var current;
+        if (this.head === null) {
+            this.head = newNode;
+        }
+        else {
+            var cur = this.head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        this.size++;
+    }
+    print() {
+        let cur = this.head;
+        while (cur != null) {
+            console.log(cur.ele + " ");
+            cur = cur.next;
+        }
+    }
+    printList() {
+        let array = ['head'];
+        let cur = this.head;
+        while (cur != null) {
+            array.push(cur.ele);
+            cur = cur.next;
+        }
+        array.push('null');
+        console.log(array.join("->"))
+    }
+    reverseLinkedList() {
+        var cur = this.head;
+        var prev = null;
+        var next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        this.head = prev;
+    }
+    rotateListByKthTimesRightSide(k) {
+        if (this.head == null) {
+            return null;
+        }
+        let len = 1;
+        var lastNode = this.head;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+            len++;
+        }
+        lastNode.next = this.head;
+        let newIndex = len - k % len;
+        for (let i = 0; i < newIndex; i++) {
+            lastNode = lastNode.next;
+        }
+        this.head = lastNode.next;
+        lastNode.next = null;
+    }
+}
+
+const list = new SingleLinkedList(5);
+
+list.append(10);
+list.append(40);
+list.append(20);
+list.append(12);
+// list.print();
+list.printList()
+list.rotateListByKthTimesRightSide(3);
+list.printList()
+
+// time complexity : o(n) as we iterating the linkedlist
+// space complexity : o(1)
